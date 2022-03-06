Fixed dcgan normalization.

models/networks.py
@@ -435,26 +435,27 @@ class DCDiscriminator(nn.Module):
         self.model = nn.Sequential(
             # input is (nc) x 256 x 256
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(ndf, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 128 x 128
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.InstanceNorm2d(ndf * 2, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 64 x 64
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.InstanceNorm2d(ndf * 4, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 32 x 32
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.InstanceNorm2d(ndf * 8, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 16 x 16
             nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 16),
+            nn.InstanceNorm2d(ndf * 16, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*16) x 8 x 8
             nn.Conv2d(ndf * 16, ndf * 32, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 32),
+            nn.InstanceNorm2d(ndf * 32, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*32) x 4 x 4
             nn.Conv2d(ndf * 32, 1, 4, 1, 0, bias=False)